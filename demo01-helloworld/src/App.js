d923aeca0da8ff76f6755ac12de0f6fad48f03a7
shiziao1996 <59598961+shiziao1996@users.noreply.github.com>
2020-04-02 17:53:34 +0800

    add project "demo01-helloworld"

---
+++ demo01-helloworld/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+// ES6语法, 相当于:
+// import React from 'react'
+// const Component = React.Component
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                hello world!
+            </div>
+        )
+    }
+}
+
+export default App;

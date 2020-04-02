0a60b3a34756e982cbfb9fc0e8e52fec40f3f0a2
shiziao1996 <59598961+shiziao1996@users.noreply.github.com>
2020-04-02 18:56:18 +0800

    add demo02

---
+++ demo02-dynamic_list/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DynamicList from './DynamicList'
+
+ReactDOM.render(
+  <DynamicList></DynamicList>,
+  document.getElementById('root')
+);
+
+
+
